6e310ba7ec30e94c540d5ad9ec0a47364239b8d6
khanhnv <vankhanh@tuoitre.com.vn>
2023-01-11 22:44:30 +0700

    Add project files.

---
+++ Bai1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap vao kich thuoc ma tran:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            var matrix = Generate(size);
+            PrintMatrix(matrix);
+        }
+
+        public static int[,] Generate(int size)
+        {
+            var random = new Random();
+            int[,] matrix = new int[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = random.Next();
+                }
+            }
+            return matrix;
+        }
+        public static void PrintMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
